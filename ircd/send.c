4df7d9d6dcabbd09c3ef9083302bddd2f3745d88
Danny Mitchell <danny@chatsystems.com>
2000-01-11 02:34:42 +0000

    Author: WildThang Log message: After discussion wit Bleep, due to the amount of controversy we'll put this back.  I guess if the admins/opers feel strong enough that wallops are being abused, they can put forth a CFV or something to either fix the operators, or change wallops however is decided. I still encourage the patch bleep wrote for use with other networks as an option. We'll leave it such that by default, it doesnt change the behavior of wallops.  Signed.... "The Threat to Undernet" *grin*. Damn I'm in a good mood today.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@15 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/send.c
+++ ircd/send.c
@@ -782,8 +782,8 @@ void sendto_ops_butone(aClient *one, aClient *from, char *pattern, ...)
   ++sentalong_marker;
   for (cptr = client; cptr; cptr = cptr->next)
   {
-    /* if (!SendWallops(cptr)) */
-    if (!IsAnOper(cptr))
+    /*if (!IsAnOper(cptr)) */
+    if (!SendWallops(cptr))
       continue;
     i = cptr->from->fd;		/* find connection oper is on */
     if (sentalong[i] == sentalong_marker)	/* sent message along it already ? */
