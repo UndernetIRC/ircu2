dac88b42e1a0967d422ca78ec8b0bdb62fbcda14
Bleep <twhelvey1@home.com>
2000-03-24 07:12:15 +0000

    Author: Bleep <tomh@inxpress.net> Log message: Fix core on RPING bug, tokenize RPING.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/branches/u2_10_10_pl1@48 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/m_rpong.c
+++ ircd/m_rpong.c
@@ -116,34 +116,48 @@ int ms_rpong(struct Client* cptr, struct Client* sptr, int parc, char* parv[])
   if (!IsServer(sptr))
     return 0;
 
-  if (parc < 5)
-    return need_more_params(sptr, "RPING");
-
-  if (!(acptr = FindClient(parv[1])))
-    return 0;
-
-  if (!IsMe(acptr))
-  {
-    if (IsServer(acptr) && parc > 5)
-    {
-      sendto_one(acptr, ":%s RPONG %s %s %s %s :%s",
-          parv[0], parv[1], parv[2], parv[3], parv[4], parv[5]);
+  if (parc < 5) {
+    /*
+     * PROTOCOL ERROR
+     */
+    return need_more_params(sptr, "RPONG");
+  }
+  if (parc == 6) {
+    /*
+     * from pinged server to source server
+     */
+    if (!(acptr = FindNServer(parv[1])))
       return 0;
+   
+    if (IsMe(acptr)) {
+      if (!(acptr = FindNUser(parv[2])))
+        return 0;
+      if (MyConnect(acptr))
+        sendto_one(acptr, ":%s " MSG_RPONG " %s %s %s :%s",
+                   me.name, acptr->name, sptr->name,
+                   militime(parv[3], parv[4]), parv[5]);
+      else 
+        sendto_one(acptr, "%s " TOK_RPONG " %s%s %s %s :%s",
+                   NumServ(&me), NumNick(acptr), sptr->name,
+                   militime(parv[3], parv[4]), parv[5]);
     }
+    else
+      sendto_one(acptr, "%s " TOK_RPONG " %s %s %s %s :%s",
+                 parv[0], parv[1], parv[2], parv[3], parv[4], parv[5]);
   }
-  else
-  {
-    parv[1] = parv[2];
-    parv[2] = sptr->name;
-    parv[0] = me.name;
-    parv[3] = militime(parv[3], parv[4]);
-    parv[4] = parv[5];
-    if (!(acptr = FindUser(parv[1])))
-      return 0;                 /* No bouncing between servers ! */
+  else {
+    /*
+     * returned from source server to client
+     */
+    if (!(acptr = FindNUser(parv[1])))
+      return 0;
+    if (MyConnect(acptr))
+      sendto_one(acptr, ":%s " MSG_RPONG " %s %s %s :%s",
+                 sptr->name, acptr->name, parv[2], parv[3], parv[4]);
+    else
+      sendto_one(acptr, "%s " TOK_RPONG " %s %s %s :%s",
+                 parv[0], parv[1], parv[2], parv[3], parv[4]);
   }
-
-  sendto_one(acptr, ":%s RPONG %s %s %s :%s",
-      parv[0], parv[1], parv[2], parv[3], parv[4]);
   return 0;
 }
 
