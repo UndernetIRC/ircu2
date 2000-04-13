e18e86e52fca1ad39e41302768ef7702d479281a
Joseph Bongaarts <foxxe@wtfs.net>
2000-04-13 21:56:24 +0000

    Author: Ghostwolf <foxxe@trms.com> Log message: Lots of stuff... Part 1 of the m_stats changes, a slew of bug fixes, cleanups for kev's G:line changes. m_stats still has more changes coming, but I wanted to make things compilable again. Added s_stats.c/h for stats helper functions. The tree compiles now, but probably won't work. There is at least 1 bug in the gline code left.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@163 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

---
+++ include/s_stats.h
@@ -0,0 +1,35 @@
+/*
+ * IRC - Internet Relay Chat, include/s_stats.h
+ * Copyright (C) 2000 Joseph Bongaarts
+ *
+ * See file AUTHORS in IRC package for additional names of
+ * the programmers.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 1, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ */
+
+#ifndef INCLUDED_s_stats_h
+#define INCLUDED_s_stats_h
+
+struct Client;
+
+extern const char *statsinfo[];
+extern void report_stats(struct Client *sptr, char stat);
+extern void report_configured_links(struct Client *sptr, int mask);
+extern int hunt_stats(struct Client* cptr, struct Client* sptr, int parc, char* parv[], char stat);
+
+#endif /* INCLUDED_s_stats_h */
