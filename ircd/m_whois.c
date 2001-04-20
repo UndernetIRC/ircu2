a9fce40285a6dc24b2fd4b499690a8c477380240
Kevin Mitchell <klmitch@mit.edu>
2001-04-20 20:56:56 +0000

    Author: Kev <klmitch@mit.edu> Log message:
    Hide server name for /who, too; allow users to see their own server name
    in /whois.  Changes still untested...
    
    
    git-svn-id: https://undernet-ircu.svn.sourceforge.net/svnroot/undernet-ircu/ircu2/trunk@427 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/m_whois.c
+++ ircd/m_whois.c
@@ -190,7 +190,7 @@ static void do_whois(struct Client* sptr, struct Client *acptr)
   }
 
 #ifdef HEAD_IN_SAND_WHOIS_SERVERNAME
-  if (!IsOper(sptr))
+  if (!IsOper(sptr) || sptr == a2cptr)
     send_reply(sptr, RPL_WHOISSERVER, name, "*.undernet.org",
 	       "The Undernet Underworld");
   else
