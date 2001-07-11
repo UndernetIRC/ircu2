c982f87bdf01f9f09606e72132c319dd57380335
Kevin L. Mitchell <klmitch@mit.edu>
2001-07-11 15:40:15 +0000

    Author: Kev <klmitch@mit.edu> Log message:
    Hopefully correct a condition that can cause the server to chew CPU by not
    clearing a particular fd--POLLHUP was not being caught.  Please test to see
    if this fixes the problem!
    
    
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@529 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/engine_devpoll.c
+++ ircd/engine_devpoll.c
@@ -62,13 +62,6 @@
 #  define POLLWRITEFLAGS POLLWRNORM
 #endif
 
-/* Figure out what bits indicate errors */
-#ifdef POLLHUP
-#  define POLLERRORS (POLLHUP|POLLERR)
-#else
-#  define POLLERRORS POLLERR
-#endif
-
 static struct Socket** sockList;
 static int devpoll_max;
 static int devpoll_fd;
@@ -317,6 +310,17 @@ engine_loop(struct Generators* gen)
 	  continue;
 	}
       }
+
+      assert(!(polls[i].revents & POLLERR));
+
+#ifdef POLLHUP
+      if (polls[i].revents & POLLHUP) { /* hang-up on socket */
+	Debug((DEBUG_ENGINE, "devpoll: EOF from client (POLLHUP)"));
+	event_generate(ET_EOF, sock, 0);
+	nfds--;
+	continue;
+      }
+#endif /* POLLHUP */
 
       switch (s_state(sock)) {
       case SS_CONNECTING:
