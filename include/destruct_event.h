d03878fd613d99ab8f5676a13543eab9fa38d4fb
Perry Lorier <isomer@undernet.org>
2002-03-07 02:07:59 +0000

    Author: Carlo Wood (Via Isomer <isomer@coders.net>) Log message:
    Added missing file.
    
    
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@663 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

---
+++ include/destruct_event.h
@@ -0,0 +1,36 @@
+#ifndef INCLUDED_destruct_event_h
+#define INCLUDED_destruct_event_h
+/*
+ * IRC - Internet Relay Chat, include/destruct_event.h
+ * Copyright (C) 2002 Carlo Wood <carlo@alinoe.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ */
+
+#ifndef INCLUDED_config_h
+#include "config.h"
+#endif
+#ifndef INCLUDED_channel_h
+#include "channel.h"
+#endif
+
+extern void schedule_destruct_event_1m(struct Channel* chptr);
+extern void schedule_destruct_event_48h(struct Channel* chptr);
+extern void remove_destruct_event(struct Channel* chptr);
+extern void exec_expired_destruct_events(struct Event* ev);
+
+#endif /* INCLUDED_destruct_event_h */
