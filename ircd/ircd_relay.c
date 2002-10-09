64c117b89c87fa9c209f017b4ac4101c07867a99
Perry Lorier <isomer@undernet.org>
2002-10-09 08:43:51 +0000

    Author: Isomer <isomer@undernet.org> Log message:
    Don't comment TOO much out :)
    
    
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/branches/u2_10_11_02@842 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/ircd_relay.c
+++ ircd/ircd_relay.c
@@ -159,15 +159,15 @@ void relay_directed_message(struct Client* sptr, char* name, char* server, const
   assert(0 != text);
   assert(0 != server);
 
-#if 0
   if ((acptr = FindServer(server + 1)) == NULL 
+#if 0
 /* X doesn't say it's a service yet! */
       || !IsService(acptr)
+#endif
       ) {
     send_reply(sptr, ERR_NOSUCHNICK, name);
     return;
   }
-#endif
   /*
    * NICK[%host]@server addressed? See if <server> is me first
    */
