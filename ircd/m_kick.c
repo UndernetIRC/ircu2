8fd5d035e793662c3e268cd94fa117347b784be8
Kevin L. Mitchell <klmitch@mit.edu>
2002-11-23 20:01:36 +0000

    Author: Kev <klmitch@mit.edu> Log message:
    Kicks from users should appear to be from users.
    
    
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/branches/u2_10_11_03@863 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/m_kick.c
+++ ircd/m_kick.c
@@ -145,8 +145,8 @@ int m_kick(struct Client *cptr, struct Client *sptr, int parc, char *parv[])
     sendcmdto_serv_butone(sptr, CMD_KICK, cptr, "%H %C :%s", chptr, who,
 			  comment);
 
-  sendcmdto_channel_butserv_butone(sptr, CMD_KICK, chptr, NULL, "%H %C :%s", chptr, who,
-			    comment);
+  sendcmdto_channel_butserv_butone(sptr, CMD_KICK, chptr, NULL, "%H %C :%s",
+				   chptr, who, comment);
 
   make_zombie(member, who, cptr, sptr, chptr);
 
@@ -227,8 +227,9 @@ int ms_kick(struct Client *cptr, struct Client *sptr, int parc, char *parv[])
 			  comment);
 
     if (member) { /* and tell the channel about it */
-      sendcmdto_channel_butserv_butone(&me, CMD_KICK, chptr, NULL, "%H %C :%s", chptr, who,
-				comment);
+      sendcmdto_channel_butserv_butone(IsServer(sptr) ? &me : sptr, CMD_KICK,
+				       chptr, NULL, "%H %C :%s", chptr, who,
+				       comment);
 
       make_zombie(member, who, cptr, sptr, chptr);
     }
