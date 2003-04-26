2d9f6a677a0d731f0f29beb3c53ce997c75b3175
Perry Lorier <isomer@undernet.org>
2003-04-26 01:23:13 +0000

    Author: David Mansell <splidge@quakenet.org> Log message:
    Allow off channel +k to invite into a channel
    
    
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/branches/u2_10_11_05@937 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/m_invite.c
+++ ircd/m_invite.c
@@ -266,10 +266,13 @@ int ms_invite(struct Client* cptr, struct Client* sptr, int parc, char* parv[])
     return 0;
   }
 
-  if (!find_channel_member(sptr, chptr)) {
-    send_reply(sptr, ERR_NOTONCHANNEL, chptr->chname);
-    return 0;
-  }
+  /* Allow remote +k users who aren't on the channel to invite people -
+   * needed for off-channel services to work properly */
+   if (!IsChannelService(sptr) && !find_channel_member(sptr, chptr)) {
+     send_reply(sptr, ERR_NOTONCHANNEL, chptr->chname);
+     return 0;
+   }
+
   if (find_channel_member(acptr, chptr)) {
     send_reply(sptr, ERR_USERONCHANNEL, cli_name(acptr), chptr->chname);
     return 0;
@@ -278,5 +281,4 @@ int ms_invite(struct Client* cptr, struct Client* sptr, int parc, char* parv[])
   sendcmdto_one(sptr, CMD_INVITE, acptr, "%s :%H", cli_name(acptr), chptr);
   return 0;
 }
-
 
