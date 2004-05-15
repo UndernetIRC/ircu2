d0a7cefb08ce59d72b9228c0b39a07ca01acf0eb
Michael Poole <mdpoole@troilus.org>
2004-05-15 15:19:09 +0000

    Forward port IPcheck bug fixes and asserts from 2.10.11.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@1051 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/IPcheck.c
+++ ircd/IPcheck.c
@@ -244,7 +244,10 @@ int ip_registry_check_local(unsigned int addr, time_t* next_target_out)
    * Don't allow more then 255 connects from one IP number, ever
    */
   if (0 == ++entry->connected)
+  {
+    entry->connected--;
     return 0;
+  }
 
   if (CONNECTED_SINCE(entry->last_connect) > IPCHECK_CLONE_PERIOD)
     entry->attempts = 0;
@@ -266,6 +269,7 @@ int ip_registry_check_local(unsigned int addr, time_t* next_target_out)
 #ifdef NOTHROTTLE 
     return 1;
 #else
+    assert(entry->connected > 0);
     --entry->connected;
     return 0;
 #endif        
@@ -337,7 +341,10 @@ void ip_registry_connect_fail(unsigned int addr)
 {
   struct IPRegistryEntry* entry = ip_registry_find(addr);
   if (entry)
-    --entry->attempts;
+  {
+    if (0 == --entry->attempts)
+      ++entry->attempts;
+  }
 }
 
 /*
@@ -392,6 +399,7 @@ void ip_registry_disconnect(struct Client *cptr)
   /*
    * If this was the last one, set `last_connect' to disconnect time (used for expiration)
    */
+  /* assert(entry->connected > 0); */
   if (0 == --entry->connected) {
     if (CONNECTED_SINCE(entry->last_connect) > IPCHECK_CLONE_LIMIT * IPCHECK_CLONE_PERIOD) {
       /*
