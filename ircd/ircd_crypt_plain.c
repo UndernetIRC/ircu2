ff6b72bed7ee097bb00c9cab922166ac7dbde43a
hikari <hikari@hikari.org.uk>
2004-09-21 15:14:33 +0000

    Author: hikari <hikari@hikari.org.uk> Log message:
    	Fixed errant * in the file comment so doxygen doesn't detect it as a documentation comment.
    
    bb
    hikari
    
    
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@1158 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/ircd_crypt_plain.c
+++ ircd/ircd_crypt_plain.c
@@ -17,9 +17,9 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  */
-/*
+/**
  * @file
- * @brief Routines for PLAIN passwords
+ * @brief Routines for PLAIN passwords.
  * @version $Id$
  * 
  * PLAIN text encryption.  Oxymoron and a half that.
