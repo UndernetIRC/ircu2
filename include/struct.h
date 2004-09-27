299971306cff7b20870d3096e3122b6ec87438cb
Michael Poole <mdpoole@troilus.org>
2004-09-27 22:34:42 +0000

    Doxyfy struct.h.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@1183 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- include/struct.h
+++ include/struct.h
@@ -17,8 +17,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- * $Id$
+ */
+/* @file
+ * @brief Structure definitions for users and servers.
+ * @version $Id$
  */
 #ifndef INCLUDED_struct_h
 #define INCLUDED_struct_h
@@ -36,47 +38,46 @@ struct User;
 struct Membership;
 struct SLink;
 
+/** Describes a server on the network. */
 struct Server {
-  struct Server*  nexts;
-  struct Client*  up;           /* Server one closer to me */
-  struct DLink*   down;         /* List with downlink servers */
-  struct DLink*   updown;       /* own Dlink in up->serv->down struct */
-  struct Client** client_list;  /* List with client pointers on this server */
-  struct User*    user;         /* who activated this connection */
-  time_t          timestamp;    /* Remotely determined connect try time */
-  time_t          ghost;        /* Local time at which a new server
+  struct Client*  up;           /**< Server one closer to me */
+  struct DLink*   down;         /**< List with downlink servers */
+  struct DLink*   updown;       /**< own Dlink in up->serv->down struct */
+  struct Client** client_list;  /**< List with client pointers on this server */
+  struct User*    user;         /**< who activated this connection */
+  time_t          timestamp;    /**< Remotely determined connect try time */
+  time_t          ghost;        /**< Local time at which a new server
                                    caused a Ghost */
-  int             lag;          /* Approximation of the amount of lag to this server */                          
-  unsigned int    clients;      /* Number of clients on the network */
-  unsigned short  prot;         /* Major protocol */
-  unsigned short  nn_last;      /* Last numeric nick for p9 servers only */
-  unsigned int    nn_mask;      /* [Remote] FD_SETSIZE - 1 */
-  char          nn_capacity[4]; /* numeric representation of server capacity */
-  
-  int            asll_rtt;     /* AsLL round-trip time */
-  int            asll_to;      /* AsLL upstream lag */
-  int            asll_from;    /* AsLL downstream lag */
+  int             lag;          /**< Approximation of the amount of lag to this server */
+  unsigned int    clients;      /**< Number of clients on the server */
+  unsigned short  prot;         /**< Major protocol */
+  unsigned int    nn_mask;      /**< Number of clients supported by server, minus 1 */
+  char          nn_capacity[4]; /**< Numeric representation of server capacity */
 
-  char *last_error_msg;         /* Allocated memory with last message receive with an ERROR */
-  char by[NICKLEN + 1];
+  int            asll_rtt;      /**< AsLL round-trip time */
+  int            asll_to;       /**< AsLL upstream lag */
+  int            asll_from;     /**< AsLL downstream lag */
+
+  char *last_error_msg;         /**< Allocated memory with last message receive with an ERROR */
+  char by[NICKLEN + 1];         /**< Numnick of client who requested the link */
 };
 
+/** Describes a user on the network. */
 struct User {
-  struct User*       nextu;
-  struct Client*     server;         /* client structure of server */
-  struct Membership* channel;        /* chain of channel pointer blocks */
-  struct SLink*      invited;        /* chain of invite pointer blocks */
-  struct SLink*      silence;        /* chain of silence pointer blocks */
-  char*              away;           /* pointer to away message */
-  time_t             last;
-  unsigned int       refcnt;          /* Number of times this block is referenced */
-  unsigned int       joined;          /* number of channels joined */
-  unsigned int       invites;         /* Number of channels we've been invited to */
-  char               username[USERLEN + 1];
-  char               host[HOSTLEN + 1];
-  char               realhost[HOSTLEN + 1];
-  char               account[ACCOUNTLEN + 1];
-  time_t	     acc_create;
+  struct Client*     server;         /**< client structure of server */
+  struct Membership* channel;        /**< chain of channel pointer blocks */
+  struct SLink*      invited;        /**< chain of invite pointer blocks */
+  struct SLink*      silence;        /**< chain of silence pointer blocks */
+  char*              away;           /**< pointer to away message */
+  time_t             last;           /**< last time user sent a message */
+  unsigned int       refcnt;         /**< Number of times this block is referenced */
+  unsigned int       joined;         /**< number of channels joined */
+  unsigned int       invites;        /**< Number of channels we've been invited to */
+  char               username[USERLEN + 1];   /**< remote account name */
+  char               host[HOSTLEN + 1];       /**< displayed hostname */
+  char               realhost[HOSTLEN + 1];   /**< actual hostname */
+  char               account[ACCOUNTLEN + 1]; /**< IRC account name */
+  time_t	     acc_create;              /**< IRC account timestamp */
 };
 
 #endif /* INCLUDED_struct_h */
