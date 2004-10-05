f7cfec05052ede2fc08a566d19d255524e0cf7d2
Michael Poole <mdpoole@troilus.org>
2004-10-05 04:11:36 +0000

    Doxyfy ircd_handler.h.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@1228 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- include/ircd_handler.h
+++ include/ircd_handler.h
@@ -16,8 +16,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- * $Id$
+ */
+/** @file
+ * @brief Message handler types and definitions.
+ * @version $Id$
  */
 #ifndef INCLUDED_ircd_handler_h
 #define INCLUDED_ircd_handler_h
@@ -27,24 +29,24 @@ struct Client;
 /*
  * MessageHandler
  */
+/** Enumerated type for client message handlers. */
 typedef enum HandlerType {
-  UNREGISTERED_HANDLER,
-  CLIENT_HANDLER,
-  SERVER_HANDLER,
-  OPER_HANDLER,
-  SERVICE_HANDLER,
-  LAST_HANDLER_TYPE
+  UNREGISTERED_HANDLER, /**< Used for unregistered clients. */
+  CLIENT_HANDLER,       /**< Used for local users. */
+  SERVER_HANDLER,       /**< Used for server conections. */
+  OPER_HANDLER,         /**< Used for IRC operators. */
+  SERVICE_HANDLER,      /**< Used for services connections. */
+  LAST_HANDLER_TYPE     /**< NUmber of handler types. */
 } HandlerType;
 
-/*
- * MessageHandler function
- * Params:
- * struct Client* cptr   - connection message originated from
- * struct Client* sptr   - source of message, may be different from cptr
- * int            parc   - parameter count
- * char*          parv[] - parameter vector
+/**
+ * MessageHandler function.
+ * @param[in] cptr Client that sent us the message.
+ * @param[in] sptr Original source of message.
+ * @param[in] parc Number of arguments.
+ * @param[in] parv Argument vector.
  */
-typedef int (*MessageHandler)(struct Client*, struct Client*, int, char*[]);
+typedef int (*MessageHandler)(struct Client* cptr, struct Client* sptr, int parc, char*parv[]);
 
 
 #endif /* INCLUDED_ircd_handler_h */
