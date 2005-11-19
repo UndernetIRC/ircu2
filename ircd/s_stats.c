ce0f2f87ef69dec7bd35138b953cc664b2185d67
Michael Poole <mdpoole@troilus.org>
2005-11-19 23:59:22 +0000

    Display IPv6 support flag with the other per-server flags.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@1569 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/s_stats.c
+++ ircd/s_stats.c
@@ -455,9 +455,9 @@ stats_servers_verbose(struct Client* sptr, const struct StatDesc* sd,
                "%-20s %-20s Flags Hops Numeric   Lag  RTT   Up Down "
                "Clients/Max Proto %-10s :Info", "Servername", "Uplink",
                "LinkTS");
-    fmt = "%-20s %-20s %c%c%c%c  %4i %s %-4i %5i %4i %4i %4i %5i %5i P%-2i   %Tu :%s";
+    fmt = "%-20s %-20s %c%c%c%c%c  %4i %s %-4i %5i %4i %4i %4i %5i %5i P%-2i   %Tu :%s";
   } else {
-    fmt = "%s %s %c%c%c%c %i %s %i %i %i %i %i %i %i P%i %Tu :%s";
+    fmt = "%s %s %c%c%c%c%c %i %s %i %i %i %i %i %i %i P%i %Tu :%s";
   }
 
   for (acptr = GlobalClientList; acptr; acptr = cli_next(acptr))
@@ -474,6 +474,7 @@ stats_servers_verbose(struct Client* sptr, const struct StatDesc* sd,
                IsBurstAck(acptr) ? 'A' : '-',
                IsHub(acptr) ? 'H' : '-',
                IsService(acptr) ? 'S' : '-',
+               IsIPv6(acptr) ? '6' : '-',
                cli_hopcount(acptr),
                NumServ(acptr),
                base64toint(cli_yxx(acptr)),
