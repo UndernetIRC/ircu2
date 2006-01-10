28aa4c7ddf8db7c14cbd0eb046610361c10cd6ca
Michael Poole <mdpoole@troilus.org>
2006-01-10 01:23:28 +0000

    Recognize ZANNELS feature for compatibility.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/branches/u2_10_12_branch@1608 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/ircd_features.c
+++ ircd/ircd_features.c
@@ -309,6 +309,7 @@ static struct FeatureDesc {
   F_S(HIDDEN_IP, 0, "127.0.0.1", 0),
   F_B(CONNEXIT_NOTICES, 0, 0, 0),
   F_B(OPLEVELS, 0, 1, 0),
+  F_B(ZANNELS, 0, 1, 0),
   F_B(LOCAL_CHANNELS, 0, 1, 0),
   F_B(TOPIC_BURST, 0, 0, 0),
 
