c84a5ed51a8a8a3d1ffeb52db7bdff5071f3c533
Michael Poole <mdpoole@troilus.org>
2006-02-05 00:52:16 +0000

    Scan all Connect blocks for next auto-reconnect time.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/branches/u2_10_12_branch@1615 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/ircd.c
+++ ircd/ircd.c
@@ -250,15 +250,18 @@ static int check_pid(void)
 static void try_connections(struct Event* ev) {
   struct ConfItem*  aconf;
   struct ConfItem** pconf;
-  time_t            next        = 0;
-  struct ConnectionClass* cltmp;
+  time_t            next;
   struct Jupe*      ajupe;
-  int hold;
+  int               hold;
+  int               done;
 
   assert(ET_EXPIRE == ev_type(ev));
   assert(0 != ev_timer(ev));
 
   Debug((DEBUG_NOTICE, "Connection check at   : %s", myctime(CurrentTime)));
+  next = CurrentTime + feature_int(FEAT_CONNECTFREQUENCY);
+  done = 0;
+
   for (aconf = GlobalConfList; aconf; aconf = aconf->next) {
     /* Only consider server items with non-zero port and non-zero
      * connect times that are not actively juped.
@@ -273,17 +276,16 @@ static void try_connections(struct Event* ev) {
     hold = aconf->hold > CurrentTime;
 
     /* Update next possible connection check time. */
-    if (hold && (next > aconf->hold || next == 0))
+    if (hold && next > aconf->hold)
         next = aconf->hold;
 
-    cltmp = aconf->conn_class;
-
     /* Do not try to connect if its use is still on hold until future,
+     * we have already initiated a connection this try_connections(),
      * too many links in its connection class, it is already linked,
      * or if connect rules forbid a link now.
      */
-    if (hold
-        || (Links(cltmp) > MaxLinks(cltmp))
+    if (hold || done
+        || (ConfLinks(aconf) > ConfMaxLinks(aconf))
         || FindServer(aconf->name)
         || conf_eval_crule(aconf->name, CRULE_MASK))
       continue;
@@ -305,14 +307,10 @@ static void try_connections(struct Event* ev) {
 			   aconf->name);
 
     /* And stop looking for further candidates. */
-    break;
+    done = 1;
   }
 
-  if (next == 0)
-    next = CurrentTime + feature_int(FEAT_CONNECTFREQUENCY);
-
   Debug((DEBUG_NOTICE, "Next connection check : %s", myctime(next)));
-
   timer_add(&connect_timer, try_connections, 0, TT_ABSOLUTE, next);
 }
 
