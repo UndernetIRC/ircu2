5d867adeda782613d5f94cbb2f59083210eb3c45
Michael Poole <mdpoole@troilus.org>
2006-05-06 23:48:26 +0000

    Avoid doing needless work for unregistered client PONGs.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/branches/u2_10_12_branch@1647 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/m_pong.c
+++ ircd/m_pong.c
@@ -162,6 +162,8 @@ int mr_pong(struct Client* cptr, struct Client* sptr, int parc, char* parv[])
   assert(cptr == sptr);
   assert(!IsRegistered(sptr));
 
+  if (!HasFlag(cptr, FLAG_PINGSENT))
+    return 0;
   ClrFlag(cptr, FLAG_PINGSENT);
   cli_lasttime(cptr) = CurrentTime;
   return (parc > 1) ? auth_set_pong(cli_auth(sptr), strtoul(parv[parc - 1], NULL, 10)) : 0;
@@ -178,6 +180,7 @@ int m_pong(struct Client* cptr, struct Client* sptr, int parc, char* parv[])
 {
   assert(0 != cptr);
   assert(cptr == sptr);
+
   ClrFlag(cptr, FLAG_PINGSENT);
   cli_lasttime(cptr) = CurrentTime;
   return 0;
