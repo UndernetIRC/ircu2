755bb393a62e7e2a964f87c850085350f765cbc8
Michael Poole <mdpoole@troilus.org>
2006-05-16 03:37:38 +0000

    Only check passwords on user ports.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@1656 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/s_auth.c
+++ ircd/s_auth.c
@@ -383,11 +383,13 @@ static int check_auth_finished(struct AuthRequest *auth)
    * as possible so that iauth's challenge/response (which uses PASS
    * for responses) is not confused with the client's password.
    */
-  if (!FlagHas(&auth->flags, AR_PASSWORD_CHECKED))
+  if (IsUserPort(auth->client)
+      && !FlagHas(&auth->flags, AR_PASSWORD_CHECKED))
   {
     struct ConfItem *aconf;
 
     aconf = cli_confs(auth->client)->value.aconf;
+    assert(aconf != NULL);
     if (!EmptyString(aconf->passwd)
         && strcmp(cli_passwd(auth->client), aconf->passwd))
     {
