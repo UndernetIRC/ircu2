460e7175eb42fb5bae92ade524fac6ae0814c849
Michael Poole <mdpoole@troilus.org>
2007-03-18 01:33:02 +0000

    Avoid extra decrements of links.inuse.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/branches/u2_10_12_branch@1781 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/list.c
+++ ircd/list.c
@@ -448,8 +448,8 @@ void free_link(struct SLink* lp)
   if (lp) {
     lp->next = slinkFreeList;
     slinkFreeList = lp;
+    links.inuse--;
   }
-  links.inuse--;
 }
 
 /** Add an element to a doubly linked list.
