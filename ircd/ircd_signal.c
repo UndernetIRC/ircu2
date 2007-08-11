622950e73981406e9077241d2a02e759dd602108
Michael Poole <mdpoole@troilus.org>
2007-08-11 21:46:49 +0000

    Get rid of code to test for restartable syscalls.
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/trunk@1826 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/ircd_signal.c
+++ ircd/ircd_signal.c
@@ -246,15 +246,6 @@ void setup_signals(void)
   signal_add(&sig_int, sigint_callback, 0, SIGINT);
   signal_add(&sig_term, sigterm_callback, 0, SIGTERM);
   signal_add(&sig_chld, sigchld_callback, 0, SIGCHLD);
-
-#ifdef HAVE_RESTARTABLE_SYSCALLS
-  /*
-   * At least on Apollo sr10.1 it seems continuing system calls
-   * after signal is the default. The following 'siginterrupt'
-   * should change that default to interrupting calls.
-   */
-  siginterrupt(SIGALRM, 1);
-#endif
 }
 
 /** Kill and clean up all child processes. */
