3c91fc4a46bc963b0e71e28a1383874a1593941c
Michael Poole <mdpoole@troilus.org>
2009-07-04 22:44:19 +0000

    Unconditionally set +6 flag on self.
    (Thanks to weibes for pointing this out on SF bug #2523774.)
    
    git-svn-id: file:///home/klmitch/undernet-ircu/undernet-ircu-svn/ircu2/branches/u2_10_12_branch@1912 c9e4aea6-c8fd-4c43-8297-357d70d61c8c

--- ircd/ircd.c
+++ ircd/ircd.c
@@ -752,9 +752,7 @@ int main(int argc, char **argv) {
   cli_lasttime(&me) = cli_since(&me) = cli_firsttime(&me) = CurrentTime;
 
   hAddClient(&me);
-#ifdef IPV6
   SetIPv6(&me);
-#endif
 
   write_pidfile();
   init_counters();
