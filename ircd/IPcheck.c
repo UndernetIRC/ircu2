UndernetIRC/ircu2

commit 9122e6f50e7f672a3c034be9f8f205a8b610ded8

IPcheck: Rate-limit connections per IPv6 /48.

ircd/IPcheck.c
@@ -55,6 +55,14 @@ struct IPRegistryEntry {
   unsigned char            attempts; /**< Number of recent connection attempts. */
 };
 
+/** Stores information about an IPv6/48 block's recent connections. */
+struct IPRegistry48 {
+  struct IPRegistry48* next;     /**< Next entry in the hash chain. */
+  int              last_connect; /**< Last connection attempt timestamp. */
+  uint16_t             addr[3];  /**< 48 MSBs of IP address. */
+  unsigned short       attempts; /**< Number of recent connection attempts. */
+};
+
 /** Size of hash table (must be a power of two). */
 #define IP_REGISTRY_TABLE_SIZE 0x10000
 /** Report current time for tracking in IPRegistryEntry::last_connect. */
@@ -66,13 +74,21 @@ struct IPRegistryEntry {
 #define IPCHECK_CLONE_LIMIT feature_int(FEAT_IPCHECK_CLONE_LIMIT)
 /** Macro for easy access to configured IPcheck clone period. */
 #define IPCHECK_CLONE_PERIOD feature_int(FEAT_IPCHECK_CLONE_PERIOD)
+/** Macro for easy access to configured /48 clone limit. */
+#define IPCHECK_48_CLONE_LIMIT feature_int(FEAT_IPCHECK_48_CLONE_LIMIT)
+/** Macro for easy access to configured 48 clone period. */
+#define IPCHECK_48_CLONE_PERIOD feature_int(FEAT_IPCHECK_48_CLONE_PERIOD)
 /** Macro for easy access to configured IPcheck clone delay. */
 #define IPCHECK_CLONE_DELAY feature_int(FEAT_IPCHECK_CLONE_DELAY)
 
 /** Hash table for storing IPRegistryEntry entries. */
 static struct IPRegistryEntry* hashTable[IP_REGISTRY_TABLE_SIZE];
+/** Hash table for storing IPRegistry48 entries. */
+static struct IPRegistry48* hashTable48[IP_REGISTRY_TABLE_SIZE];
 /** List of allocated but unused IPRegistryEntry structs. */
 static struct IPRegistryEntry* freeList;
+/** List of allocated but unused IPRegistry48 structs. */
+static struct IPRegistry48* freeList48;
 /** Periodic timer to look for too-old registry entries. */
 static struct Timer expireTimer;
 
@@ -239,6 +255,57 @@ static void ip_registry_expire_entry(struct IPRegistryEntry* entry)
   }
 }
 
+/** Calculate hash value for an IP address's /48 block.
+ * @param[in] ip Address to hash; must be an IPv6 address.
+ * @return Hash value for address.
+ */
+static unsigned int ip_48_hash(const struct irc_in_addr *ip)
+{
+  unsigned int res;
+  res = ip->in6_16[0] ^ ip->in6_16[1] ^ ip->in6_16[2];
+  return res & (IP_REGISTRY_TABLE_SIZE - 1);
+}
+
+/** Find or create an IPv6 /48 entry for the IP address.
+ * @param[in] ip IPv6 address to search for.
+ * @return Matching registry entry (possibly newly created).
+ */
+static struct IPRegistry48* ip_48_find(const struct irc_in_addr *ip)
+{
+  struct IPRegistry48* entry;
+  unsigned int idx;
+
+  /* Does it exist in the chain? */
+  idx = ip_48_hash(ip);
+  for (entry = hashTable48[idx]; entry; entry = entry->next) {
+    if ((ip->in6_16[0] == entry->addr[0])
+        && (ip->in6_16[1] == entry->addr[1])
+        && (ip->in6_16[2] == entry->addr[2]))
+      goto done;
+  }
+
+  /* Where do we get the next entry? */
+  entry = freeList48;
+  if (entry)
+    freeList48 = entry->next;
+  else
+    entry = MyMalloc(sizeof(*entry));
+
+  /* Initialize this entry. */
+  entry->last_connect = NOW;
+  entry->addr[0]  = ip->in6_16[0];
+  entry->addr[1]  = ip->in6_16[1];
+  entry->addr[2]  = ip->in6_16[2];
+  entry->attempts = 0;
+
+  /* Link it into the hash table. */
+  entry->next = hashTable48[idx];
+  hashTable48[idx] = entry;
+
+done:
+  return entry;
+}
+
 /** Periodic timer callback to check for expired registry entries.
  * @param[in] ev Timer event (ignored).
  */
@@ -256,6 +323,20 @@ static void ip_registry_expire(struct Event* ev)
       entry_next = entry->next;
       if (0 == entry->connected)
         ip_registry_expire_entry(entry);
+    }
+  }
+
+  for (i = 0; i < IP_REGISTRY_TABLE_SIZE; ++i) {
+    struct IPRegistry48** prev_p;
+    struct IPRegistry48* entry;
+
+    for (prev_p = &hashTable48[i]; (entry = *prev_p); ) {
+      if (CONNECTED_SINCE(entry->last_connect) > 600) {
+        *prev_p = entry->next;
+        entry->next = freeList48;
+        freeList48 = entry;
+      } else
+        prev_p = &entry->next;
     }
   }
 }
@@ -278,6 +359,23 @@ int ip_registry_check_local(const struct irc_in_addr *addr, time_t* next_target_
 {
   struct IPRegistryEntry* entry = ip_registry_find(addr);
   unsigned int free_targets = STARTTARGETS;
+
+  if (!irc_in_addr_is_ipv4(addr)) {
+    struct IPRegistry48* entry_48 = ip_48_find(addr);
+
+    if (CONNECTED_SINCE(entry_48->last_connect) > IPCHECK_48_CLONE_PERIOD)
+      entry_48->attempts = 0;
+
+    entry_48->last_connect = NOW;
+    if (0 == ++entry_48->attempts)
+      --entry_48->attempts;
+
+#ifndef NOTHROTTLE
+    if ((entry_48->attempts >= IPCHECK_48_CLONE_LIMIT)
+        && ((CurrentTime - cli_since(&me) > IPCHECK_CLONE_DELAY)))
+      goto reject;
+#endif
+  }
 
   if (0 == entry) {
     entry       = ip_registry_new_entry();
@@ -310,17 +408,18 @@ int ip_registry_check_local(const struct irc_in_addr *addr, time_t* next_target_
     if (next_target_out)
       *next_target_out = CurrentTime - (TARGET_DELAY * free_targets - 1);
   }
+#ifndef NOTHROTTLE
   else if ((CurrentTime - cli_since(&me)) > IPCHECK_CLONE_DELAY) {
     /*
      * Don't refuse connection when we just rebooted the server
      */
-#ifndef NOTHROTTLE
+reject:
     assert(entry->connected > 0);
     --entry->connected;
     Debug((DEBUG_DNS, "IPcheck refusing local connection from %s: too fast.", ircd_ntoa(&entry->addr)));
     return 0;
-#endif
   }
+#endif
   Debug((DEBUG_DNS, "IPcheck accepting local connection from %s.", ircd_ntoa(&entry->addr)));
   return 1;
 }
@@ -345,6 +444,16 @@ int ip_registry_check_remote(struct Client* cptr, int is_burst)
     Debug((DEBUG_DNS, "IPcheck accepting remote connection from invalid %s.", ircd_ntoa(&cli_ip(cptr))));
     return 1;
   }
+
+  if (!irc_in_addr_is_ipv4(&cli_ip(cptr))) {
+    struct IPRegistry48* entry_48 = ip_48_find(&cli_ip(cptr));
+    if (CONNECTED_SINCE(entry_48->last_connect) > IPCHECK_48_CLONE_PERIOD)
+      entry_48->attempts = 0;
+    if (0 == ++entry_48->attempts)
+      --entry_48->attempts;
+    entry_48->last_connect = NOW;
+  }
+
   entry = ip_registry_find(&cli_ip(cptr));
   if (0 == entry) {
     entry = ip_registry_new_entry();
@@ -385,6 +494,13 @@ int ip_registry_check_remote(struct Client* cptr, int is_burst)
 void ip_registry_connect_fail(const struct irc_in_addr *addr, int disconnect)
 {
   struct IPRegistryEntry* entry = ip_registry_find(addr);
+
+  if (!irc_in_addr_is_ipv4(addr)) {
+    struct IPRegistry48* entry_48 = ip_48_find(addr);
+    if (0 == --entry_48->attempts)
+      ++entry_48->attempts;
+  }
+
   if (entry) {
     if (0 == --entry->attempts) {
       Debug((DEBUG_DNS, "IPcheck noting local connection failure for %s.", ircd_ntoa(&entry->addr)));

doc/readme.features
@@ -839,6 +839,22 @@ to 10, then a user is only allowed to connect once in 10s, if they connect
 again within 10s, then they are considered to be connecting too fast and
 they are throttled.
 
+IPCHECK_48_CLONE_LIMIT
+ * Type: integer
+ * Default: 50
+
+This is a limit that works like IPCHECK_CLONE_LIMIT, but for all
+clients connecting from a given IPv6 /48 block.
+
+IPCHECK_48_CLONE_PERIOD
+ * Type: integer
+ * Default: 10
+
+This is a timer that works like IPCHECK_CLONE_PERIOD, but for all
+clients connecting from a given IPv6 /48 block.  The number of
+connection attempts allowed in this period is given by the
+IPCHECK_48_CLONE_LIMIT feature.
+
 IPCHECK_CLONE_DELAY
  * Type: integer
  * Default: 600

include/ircd_features.h
@@ -80,6 +80,8 @@ enum Feature {
   FEAT_SOCKRECVBUF,
   FEAT_IPCHECK_CLONE_LIMIT,
   FEAT_IPCHECK_CLONE_PERIOD,
+  FEAT_IPCHECK_48_CLONE_LIMIT,
+  FEAT_IPCHECK_48_CLONE_PERIOD,
   FEAT_IPCHECK_CLONE_DELAY,
   FEAT_CHANNELLEN,
 

ircd/ircd_features.c
@@ -345,6 +345,8 @@ static struct FeatureDesc {
   F_I(SOCKRECVBUF, 0, SERVER_TCP_WINDOW, 0),
   F_I(IPCHECK_CLONE_LIMIT, 0, 4, 0),
   F_I(IPCHECK_CLONE_PERIOD, 0, 40, 0),
+  F_I(IPCHECK_48_CLONE_LIMIT, 0, 50, 0),
+  F_I(IPCHECK_48_CLONE_PERIOD, 0, 10, 0),
   F_I(IPCHECK_CLONE_DELAY, 0, 600, 0),
   F_I(CHANNELLEN, 0, 200, 0),
 

commit 6e71519769645a9baef50a2c469f887bb1d2d69b

IPcheck: Bugfix for IPv6 /48 rejections from a new /64.
After "IPcheck: Rate-limit connections per IPv6 /48.", if a connection
from a rate-limited /48 came from a /64 that had no current connections,
then `entry` can be null in this path.

Fixes: 9122e6f50e7f672a3c034be9f8f205a8b610ded8

ircd/IPcheck.c
@@ -414,8 +414,11 @@ int ip_registry_check_local(const struct irc_in_addr *addr, time_t* next_target_
      * Don't refuse connection when we just rebooted the server
      */
 reject:
-    assert(entry->connected > 0);
-    --entry->connected;
+    if (entry)
+    {
+      assert(entry->connected > 0);
+      --entry->connected;
+    }
     Debug((DEBUG_DNS, "IPcheck refusing local connection from %s: too fast.", ircd_ntoa(&entry->addr)));
     return 0;
   }