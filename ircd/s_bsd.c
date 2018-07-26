UndernetIRC/ircu2

commit 48c15bd1cf3a9a2057924a0a1e4a0aca4db4a6c4

s_bsd: Fix loop and comparison of dolen.

ircd/s_bsd.c
@@ -663,12 +663,10 @@ static int read_packet(struct Client *cptr, int socket_ready)
        */
       if (IsHandshake(cptr) || IsServer(cptr))
       {
-        while (-1)
+        while (1)
         {
           dolen = dbuf_get(&(cli_recvQ(cptr)), readbuf, sizeof(readbuf));
-          if (dolen <= 0)
-            return 1;
-          else if (dolen == 0)
+          if (dolen == 0)
           {
             if (DBufLength(&(cli_recvQ(cptr))) < 510)
               SetFlag(cptr, FLAG_NONL);

commit 62c604b90202ea67a70148e3d227707608bf4f8f

s_bsd: Fix the dolen comparison bugfix, and clean up nearby code.
Fixes: 48c15bd1cf3a9a2057924a0a1e4a0aca4db4a6c4

ircd/s_bsd.c
@@ -667,16 +667,10 @@ static int read_packet(struct Client *cptr, int socket_ready)
         {
           dolen = dbuf_get(&(cli_recvQ(cptr)), readbuf, sizeof(readbuf));
           if (dolen == 0)
-          {
-            if (DBufLength(&(cli_recvQ(cptr))) < 510)
-              SetFlag(cptr, FLAG_NONL);
-            else
-              DBufClear(&(cli_recvQ(cptr)));
-          }
-          else if ((IsServer(cptr) &&
-                    server_dopacket(cptr, readbuf, dolen) == CPTR_KILLED) ||
-                   (!IsServer(cptr) &&
-                    connect_dopacket(cptr, readbuf, dolen) == CPTR_KILLED))
+            return 1;
+          if ((IsServer(cptr)
+               ? server_dopacket(cptr, readbuf, dolen)
+               : connect_dopacket(cptr, readbuf, dolen)) == CPTR_KILLED)
             return CPTR_KILLED;
         }
       }