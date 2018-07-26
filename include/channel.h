27eb19203da9b2c149431f8616f0f15bf847bc37
Michael Poole <mdpoole@troilus.org>
2018-07-25 21:06:37 -0400

    channel.h: MODE_REGISTERED uses +R, it is no longer for future expansion.

--- include/channel.h
+++ include/channel.h
@@ -101,8 +101,7 @@ struct Client;
 #define MODE_LIMIT      0x0400		/**< +l Limit */
 #define MODE_REGONLY    0x0800  	/**< Only +r users may join */
 #define MODE_DELJOINS   0x1000  	/**< New join messages are delayed */
-#define MODE_REGISTERED 0x2000  	/**< Channel marked as registered
-					 * (for future semantic expansion) */
+#define MODE_REGISTERED 0x2000  	/**< +R registered with services */
 #define MODE_NOCOLOR    0x4000          /**< +c No colors */
 #define MODE_NOCTCP     0x8000          /**< +C No CTCPs except ACTION */
 #define MODE_SAVE	0x20000		/**< save this mode-with-arg 'til
