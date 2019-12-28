UndernetIRC/ircu2

commit c59e22b2636a1fcb8d9453dae5df638a8ddebb65

vdebug: Use the caller-provided level for logging.

ircd/s_debug.c
@@ -153,7 +153,7 @@ void vdebug(int level, const char *form, va_list vl)
   if (!loop && (debuglevel >= 0) && (level <= debuglevel))
   {
     loop = 1;
-    log_vwrite(LS_DEBUG, L_DEBUG, 0, form, vl);
+    log_vwrite(LS_DEBUG, level, 0, form, vl);
     loop = 0;
   }
   errno = err;

commit 409bb69d637882304db359e1b24fc6114bd95077

vdebug: Clamp log level to L_DEBUG
Fixes: c59e22b2636a1fcb8d9453dae5df638a8ddebb65

ircd/s_debug.c
@@ -149,11 +149,14 @@ void vdebug(int level, const char *form, va_list vl)
 {
   static int loop = 0;
   int err = errno;
+  int l_level = level;
+  if (l_level >= L_LAST_LEVEL)
+    l_level = L_DEBUG;
 
   if (!loop && (debuglevel >= 0) && (level <= debuglevel))
   {
     loop = 1;
-    log_vwrite(LS_DEBUG, level, 0, form, vl);
+    log_vwrite(LS_DEBUG, l_level, 0, form, vl);
     loop = 0;
   }
   errno = err;