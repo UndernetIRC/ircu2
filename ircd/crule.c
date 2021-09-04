UndernetIRC/ircu2

commit 3ce5623b9ebda6614229e51927260333245bd034

crule_parseprimary: Remove redundant return.

ircd/crule.c
@@ -637,7 +637,6 @@ static int crule_parseprimary(CRuleNodePtr* primrootp, int *next_tokp, const cha
           errcode = CR_EXPCTPRIM;
         break;
     }
-    return (errcode);
   }
   return (errcode);
 }

commit 3352dc90498bd68b775f9b3de2ad48bb7d17ddfa

Document the overly complex crule_parseprimary()
Fixes: 3ce5623b9ebda6614229e51927260333245bd034

ircd/crule.c
@@ -637,6 +637,7 @@ static int crule_parseprimary(CRuleNodePtr* primrootp, int *next_tokp, const cha
           errcode = CR_EXPCTPRIM;
         break;
     }
+    break; /* loop only continues after a CR_NOT */
   }
   return (errcode);
 }