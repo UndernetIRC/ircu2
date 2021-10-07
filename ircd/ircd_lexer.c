844238f437d508a63e008add37350bfbb8fb01c4
Michael Poole <mdpoole@troilus.org>
2021-10-07 19:53:01 -0400

    Use a hand-coded lexical analyzer
    This does not change any behavior yet, but allows us to implement file
    inclusion, which is not supported by POSIX lex.

---
+++ ircd/ircd_lexer.c
@@ -0,0 +1,383 @@
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "config.h"
+#include "ircd.h"  /* configfile */
+#include "ircd_alloc.h"
+#include "ircd_log.h"
+#include "ircd_string.h" /* ircd_strcmp() */
+#include "y.tab.h"
+
+#if !defined(O_CLOEXEC)
+# define O_CLOEXEC O_RDONLY
+#endif
+
+/* NOTE: This file uses <ctype.h> and isalnum() rather than ircd_chattr.h
+ * and IsAlnum() because IsAlnum() returns true for "{|}~[\\]^".
+ */
+
+/** Represents one config file. */
+struct lex_file
+{
+  /** Config file that included this one. */
+  struct lex_file *parent;
+
+  /** Name of this config file. */
+  char *name;
+
+  /** File descriptor for this config file. */
+  int fd;
+
+  /** Current line number in this config file. */
+  int lineno;
+
+  /** Offset in #buf of the token we are looking at.
+   * When yyinput() is not running, this is just past the end of the
+   * last token, and may be equal to #buf_used.
+   */
+  int tok_ofs;
+
+  /** Number of bytes currently valid in #buf. */
+  int buf_used;
+
+  /** Current buffer of input from the file. */
+  char buf[1024];
+};
+
+struct lexer_token {
+  const char *string;
+  int value;
+};
+
+/* WARNING: tokens MUST be lower case and lexicographically sorted */
+static const struct lexer_token tokens[] = {
+  { "admin", ADMIN },
+  { "administrator", ADMIN },
+  { "all", ALL },
+  { "apass_opmode", TPRIV_APASS_OPMODE },
+  { "auto", AUTOCONNECT },
+  { "autoconnect", AUTOCONNECT },
+  { "b", BYTES },
+  { "badchan", TPRIV_BADCHAN },
+  { "bytes", BYTES },
+  { "chan_limit", TPRIV_CHAN_LIMIT },
+  { "channel", CHANNEL },
+  { "class", CLASS },
+  { "client", CLIENT },
+  { "connect", CONNECT },
+  { "connectfreq", CONNECTFREQ },
+  { "contact", CONTACT },
+  { "crule", CRULE },
+  { "days", DAYS },
+  { "decades", DECADES },
+  { "deop_lchan", TPRIV_DEOP_LCHAN },
+  { "description", DESCRIPTION },
+  { "die", TPRIV_DIE },
+  { "display", TPRIV_DISPLAY },
+  { "dns", DNS },
+  { "except", EXCEPT },
+  { "fast", FAST },
+  { "features", FEATURES },
+  { "file", TFILE },
+  { "force_local_opmode", TPRIV_FORCE_LOCAL_OPMODE },
+  { "force_opmode", TPRIV_FORCE_OPMODE },
+  { "gb", GBYTES },
+  { "gbytes", GBYTES },
+  { "general", GENERAL },
+  { "gigabytes", GBYTES },
+  { "gline", TPRIV_GLINE },
+  { "hidden", HIDDEN },
+  { "host", HOST },
+  { "hours", HOURS },
+  { "hub", HUB },
+  { "iauth", IAUTH },
+  { "ip", IP },
+  { "ipcheck", IPCHECK },
+  { "ipv4", TOK_IPV4 },
+  { "ipv6", TOK_IPV6 },
+  { "jupe", JUPE },
+  { "kb", KBYTES },
+  { "kbytes", KBYTES },
+  { "kill", KILL },
+  { "kilobytes", KBYTES },
+  { "leaf", LEAF },
+  { "list_chan", TPRIV_LIST_CHAN },
+  { "local", LOCAL },
+  { "local_badchan", TPRIV_LOCAL_BADCHAN },
+  { "local_gline", TPRIV_LOCAL_GLINE },
+  { "local_jupe", TPRIV_LOCAL_JUPE },
+  { "local_kill", TPRIV_LOCAL_KILL },
+  { "local_opmode", TPRIV_LOCAL_OPMODE },
+  { "location", LOCATION },
+  { "mask", MASK },
+  { "maxhops", MAXHOPS },
+  { "maxlinks", MAXLINKS },
+  { "mb", MBYTES },
+  { "mbytes", MBYTES },
+  { "megabytes", MBYTES },
+  { "minutes", MINUTES },
+  { "mode_lchan", TPRIV_MODE_LCHAN },
+  { "months", MONTHS },
+  { "motd", MOTD },
+  { "name", NAME },
+  { "nick", NICK },
+  { "no", NO },
+  { "numeric", NUMERIC },
+  { "oper", OPER },
+  { "operator", OPER },
+  { "opmode", TPRIV_OPMODE },
+  { "pass", PASS },
+  { "password", PASS },
+  { "pingfreq", PINGFREQ },
+  { "port", PORT },
+  { "prepend", PREPEND },
+  { "program", PROGRAM },
+  { "propagate", TPRIV_PROPAGATE },
+  { "pseudo", PSEUDO },
+  { "quarantine", QUARANTINE },
+  { "real", REAL },
+  { "realname", REAL },
+  { "reason", REASON },
+  { "rehash", TPRIV_REHASH },
+  { "restart", TPRIV_RESTART },
+  { "rule", RULE },
+  { "seconds", SECONDS },
+  { "see_chan", TPRIV_SEE_CHAN },
+  { "see_opers", TPRIV_SEE_OPERS },
+  { "sendq", SENDQ },
+  { "server", SERVER },
+  { "set", TPRIV_SET },
+  { "show_all_invis", TPRIV_SHOW_ALL_INVIS },
+  { "show_invis", TPRIV_SHOW_INVIS },
+  { "tb", TBYTES },
+  { "tbytes", TBYTES },
+  { "terabytes", TBYTES },
+  { "timeout", TIMEOUT },
+  { "unlimit_query", TPRIV_UNLIMIT_QUERY },
+  { "usermode", USERMODE },
+  { "username", USERNAME },
+  { "uworld", UWORLD },
+  { "vhost", VHOST },
+  { "walk_lchan", TPRIV_WALK_LCHAN },
+  { "webirc", WEBIRC },
+  { "weeks", WEEKS },
+  { "whox", TPRIV_WHOX },
+  { "wide_gline", TPRIV_WIDE_GLINE },
+  { "years", YEARS },
+  { "yes", YES },
+  { NULL, 0 }
+};
+static const int ntokens = sizeof(tokens) / sizeof(tokens[0]) - 1;
+static struct lex_file *yy_in;
+extern int lineno;
+
+static int lexer_open(const char *fname, int allow_fail)
+{
+  struct lex_file *obj;
+
+  obj = MyMalloc(sizeof(*obj));
+  obj->fd = open(fname, O_RDONLY | O_NOCTTY | O_CLOEXEC);
+  if (obj->fd < 0) {
+    if (!allow_fail) {
+      log_write(LS_CONFIG, L_CRIT, 0, "unable to open config file: %s", fname);
+    }
+    MyFree(obj);
+    return -1;
+  }
+
+  DupString(obj->name, fname);
+  obj->parent = yy_in;
+  obj->lineno = 1;
+  obj->tok_ofs = obj->buf_used = 0;
+  yy_in = obj;
+  return 0;
+}
+
+static void lexer_pop(void)
+{
+  struct lex_file *obj;
+
+  obj = yy_in;
+  yy_in = obj->parent;
+  close(obj->fd);
+  MyFree(obj->name);
+  MyFree(obj);		
+}
+
+int init_lexer(void)
+{
+#if !defined(NDEBUG)
+  int ii, jj;
+
+  assert(!yy_in);
+
+  for (ii = 0; ii < ntokens; ++ii) {
+    char ch;
+
+    for (jj = 0; (ch = tokens[ii].string[jj]) != '\0'; ++jj) {
+      if (!islower(ch) && !isdigit(ch) && ch != '_') {
+        log_write(LS_CONFIG, L_CRIT, 0, "token %s is not lower case",
+          tokens[ii].string);
+      }
+    }
+
+    if (ii > 0 && ircd_strcmp(tokens[ii-1].string, tokens[ii].string) >= 0) {
+      log_write(LS_CONFIG, L_CRIT, 0, "token ordering error: %s >= %s",
+        tokens[ii-1].string, tokens[ii].string);
+    }
+  }
+#endif
+
+  return lexer_open(configfile, 1) == 0; /* we return non-zero on success */
+}
+
+void deinit_lexer(void)
+{
+  assert(!yy_in);
+
+  while (yy_in) {
+    lexer_pop();
+  }
+}
+
+static int token_compare(const void *key, const void *ptok)
+{
+  const char *word = key;
+  const struct lexer_token *tok = ptok;
+  unsigned int ii = 0;
+  while (word[ii] && (tolower(word[ii]) == tok->string[ii]))
+    ii++;
+  return tolower(word[ii]) - tok->string[ii];
+}
+
+static int find_token(char *token)
+{
+  struct lexer_token *tok;
+  tok = bsearch(token, tokens, ntokens, sizeof(tokens[0]), token_compare);
+  return tok ? tok->value : 0;
+}
+
+int yylex(void)
+{
+  char *pos, *stop, *start;
+  ssize_t nbr;
+  char save;
+
+  if (!yy_in)
+    return 0;
+
+  for (;;) {
+    pos = yy_in->buf + yy_in->tok_ofs;
+    stop = yy_in->buf + yy_in->buf_used;
+
+    /* Skip whitespace at the start of the buffer. */
+    while ((pos < stop) && isspace(*pos)) {
+      if (*pos++ == '\n')
+        ++yy_in->lineno;
+    }
+    yy_in->tok_ofs = pos - yy_in->buf;
+
+    /* If we are looking at a comment, chomp through EOL. */
+    if ((pos < stop) && (*pos == '#')) {
+      while ((++pos < stop) && (*pos != '\n')) {}
+      if (pos == stop)
+        goto grab_more;
+      yy_in->tok_ofs = ++pos - yy_in->buf;
+      ++yy_in->lineno;
+      continue;
+    }
+
+    /* Are we looking at a quoted string? */
+    if ((pos < stop) && (*pos == '"')) {
+      start = pos;
+      while ((++pos < stop) && (*pos != '\n') && (*pos != '"')) {}
+      if (pos == stop)
+        goto grab_more;
+      if (*pos == '\n') {
+        log_write(LS_CONFIG, L_CRIT, 0, "newline in quoted string at line %d of %s",
+          yy_in->lineno, yy_in->name);
+      }
+      *pos++ = '\0';
+      DupString(yylval.text, start + 1);
+      yy_in->tok_ofs = pos - yy_in->buf;
+      lineno = yy_in->lineno;
+      return QSTRING;
+    }
+
+    /* Are we looking at a number? */
+    if ((pos < stop) && isdigit(*pos)) {
+      int num = 0;
+      do {
+        num = (num * 10) + *pos - '0';
+      } while ((++pos < stop) && isdigit(*pos));
+      if (pos == stop)
+        goto grab_more;
+      yy_in->tok_ofs = pos - yy_in->buf;
+      yylval.num = num;
+      lineno = yy_in->lineno;
+      return NUMBER;
+    }
+
+    /* Are we looking at a bare token? */
+    if ((pos < stop) && isalpha(*pos)) {
+      start = pos;
+      while ((++pos < stop) && (isalnum(*pos) || *pos == '_')) {}
+      if (pos == stop)
+        goto grab_more;
+      save = *pos;
+      *pos = '\0';
+      nbr = find_token(start);
+      *pos = save;
+      yy_in->tok_ofs = pos - yy_in->buf;
+      lineno = yy_in->lineno;
+      if (nbr)
+        return nbr;
+      log_write(LS_CONFIG, L_CRIT, 0, "unhandled token %s at line %d of %s",
+        start, yy_in->lineno, yy_in->name);
+    }
+
+    /* Otherwise we are looking at a meaningful character. */
+    if (pos < stop) {
+      yy_in->tok_ofs = ++pos - yy_in->buf;
+      lineno = yy_in->lineno;
+      return pos[-1];
+    }
+
+  grab_more:
+    /* Shift the live part of the buffer to the start. */
+    if (yy_in->tok_ofs > 0) {
+      assert(yy_in->tok_ofs <= yy_in->buf_used);
+      yy_in->buf_used -= yy_in->tok_ofs;
+      memmove(yy_in->buf, yy_in->buf + yy_in->tok_ofs, yy_in->buf_used);
+      yy_in->tok_ofs = 0;
+    }
+
+    /* Do we have space to read more data from disk? */
+    if (yy_in->buf_used >= sizeof(yy_in->buf)) {
+      log_write(LS_CONFIG, L_CRIT, 0, "token at line %d of %s too big",
+        yy_in->lineno, yy_in->name);
+    }
+
+    /* Read more data into the buffer. */
+    nbr = read(yy_in->fd, yy_in->buf + yy_in->buf_used,
+      sizeof(yy_in->buf) - yy_in->buf_used);
+    if (nbr == 0) {
+      lexer_pop();
+      if (!yy_in)
+        return 0;
+      /* TODO: return end-of-included-file token */
+    }
+    if (nbr < 0) {
+      log_write(LS_CONFIG, L_ERROR, 0, "read from %s failed: %s",
+        yy_in->name, strerror(errno));
+      return 0;
+    }
+    yy_in->buf_used += nbr;
+    assert(yy_in->buf_used <= sizeof(yy_in->buf));
+  }
+}
