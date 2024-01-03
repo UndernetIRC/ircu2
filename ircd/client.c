UndernetIRC/ircu2

commit b9c579031c1df986f906608543fed0a276f382a1

Copy max sendQ for remotely opered clients

ircd/client.c
@@ -185,6 +185,12 @@ client_set_privs(struct Client *client, struct ConfItem *oper, int forceOper)
     if (class && (!FlagHas(&class->privs_dirty, PRIV_PROPAGATE)
               || !FlagHas(&class->privs, PRIV_PROPAGATE)))
       class = NULL;
+
+    /* For remote opers, we need to also set their max sendq because
+     * we will not be attaching an Operator block to their client.
+     */
+    if (MaxSendq(class))
+      cli_max_sendq(client) = MaxSendq(class);
   }
 
   /* Decide whether to use global or local oper defaults. */

commit f8f62f8cb355fd083bbdce33ea1bb01a4b8c3def

Only apply MaxSendq when a class exists
Fixes: b9c579031c1df986f906608543fed0a276f382a1

ircd/client.c
@@ -189,7 +189,7 @@ client_set_privs(struct Client *client, struct ConfItem *oper, int forceOper)
     /* For remote opers, we need to also set their max sendq because
      * we will not be attaching an Operator block to their client.
      */
-    if (MaxSendq(class))
+    if (class && MaxSendq(class))
       cli_max_sendq(client) = MaxSendq(class);
   }
 