e87beedcf44b8e604771bd0521dda4733311d47a
Empus <empus@undernet.org>
2024-03-21 22:14:08 +1000

    Update supported.h for chanmode +P

--- include/supported.h
+++ include/supported.h
@@ -66,8 +66,8 @@
                         feature_int(FEAT_CHANNELLEN), CHANNELLEN, \
                         (feature_bool(FEAT_LOCAL_CHANNELS) ? "#&" : "#"), "(ov)@+", "@+", \
                         (feature_bool(FEAT_OPLEVELS) \
-                        ? "b,AkU,l,imnpstrDdRcCM" \
-                        :   "b,k,l,imnpstrDdRcCM"), \
+                        ? "b,AkU,l,imnpstrDdRcCPM" \
+                        :   "b,k,l,imnpstrDdRcCPM"), \
                         "rfc1459", feature_str(FEAT_NETWORK)
 
 #endif /* INCLUDED_supported_h */
