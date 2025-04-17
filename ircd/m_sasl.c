a91ebc6c3fef31078291a36ee25fad7c2e129e41
Andreas Pschorn <gh@midiandmore.de>
2025-04-17 20:11:58 +0200

    Update m_sasl.c

--- ircd/m_sasl.c
+++ ircd/m_sasl.c
@@ -19,7 +19,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- * $Id: m_sasl.c,v 1.3.2.1 2025/04/01 04:04:32 entrope Exp $
+ * $Id$
  */
 
 /*
